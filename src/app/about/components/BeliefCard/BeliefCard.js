8159a0b6aa5ca813d23c48faea592ce4a3804f62
Xiaow7123 <qi.xiaow@northeastern.edu>
2025-11-05 16:07:56 -0800

    Restore about page functionality after syncing with develop

---
+++ src/app/about/components/BeliefCard/BeliefCard.js
@@ -0,0 +1,11 @@
+import styles from './BeliefCard.module.css'
+
+export default function BeliefCard({ icon, title, description }) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.icon}>{icon}</div>
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
+    </div>
+  )
+}
